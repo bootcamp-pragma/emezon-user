470922773e6d6ce7cf6af41a6ab83636877bfcca
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-09-09 15:52:54 -0500

    Created IRolRepositoryOutPort

---
+++ src/main/java/com/emezon/user/domain/ports/outbound/IRolRepositoryOutPort.java
@@ -0,0 +1,18 @@
+package com.emezon.user.domain.ports.outbound;
+
+import com.emezon.user.domain.models.Rol;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IRolRepositoryOutPort {
+
+    Rol save(Rol rol);
+
+    Optional<Rol> findById(String id);
+
+    Optional<Rol> findByName(String name);
+
+    List<Rol> findAll();
+
+}
