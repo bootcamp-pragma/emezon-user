d2bd3258b210d0ad78ff40afd3ea5d0c19522b71
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-09-10 15:26:46 -0500

    Created MySQLJPARolAdapter

---
+++ src/main/java/com/emezon/user/infra/outbound/mysql/jpa/adapters/MySQLJPARolAdapter.java
@@ -0,0 +1,46 @@
+package com.emezon.user.infra.outbound.mysql.jpa.adapters;
+
+
+import com.emezon.user.domain.models.Rol;
+import com.emezon.user.domain.ports.outbound.IRolRepositoryOutPort;
+import com.emezon.user.infra.outbound.mysql.jpa.entities.RolEntity;
+import com.emezon.user.infra.outbound.mysql.jpa.mappers.RolEntityMapper;
+import com.emezon.user.infra.outbound.mysql.jpa.repositories.IMySQLJPARolRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class MySQLJPARolAdapter implements IRolRepositoryOutPort {
+
+    private final IMySQLJPARolRepository mySQLJPARolRepository;
+
+
+    @Override
+    public Rol save(Rol rol) {
+        RolEntity rolEntity = RolEntityMapper.toEntity(rol);
+        RolEntity savedRolEntity = mySQLJPARolRepository.save(rolEntity);
+        return RolEntityMapper.toDomain(savedRolEntity);
+    }
+
+    @Override
+    public Optional<Rol> findById(String id) {
+        Optional<RolEntity> rolEntity = mySQLJPARolRepository.findById(id);
+        return rolEntity.map(RolEntityMapper::toDomain);
+    }
+
+    @Override
+    public Optional<Rol> findByName(String name) {
+        Optional<RolEntity> rolEntity = mySQLJPARolRepository.findByName(name);
+        return rolEntity.map(RolEntityMapper::toDomain);
+    }
+
+    @Override
+    public List<Rol> findAll() {
+        List<RolEntity> rolEntities = mySQLJPARolRepository.findAll();
+        return rolEntities.stream().map(RolEntityMapper::toDomain).toList();
+    }
+}
--- src/main/java/com/emezon/user/infra/outbound/mysql/jpa/repositories/IMySQLJPARolRepository.java
+++ src/main/java/com/emezon/user/infra/outbound/mysql/jpa/repositories/IMySQLJPARolRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IMySQLJPARolRepository extends JpaRepository<RolEntity, String> {
 
     @Query("SELECT r FROM roles r WHERE LOWER(r.name)  = LOWER(:name)")
-    RolEntity findByName(String name);
+    Optional<RolEntity> findByName(String name);
 
 }
