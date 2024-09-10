d4fcdeb43d5589023f5fa168dc3d5793b6248282
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-09-10 15:40:54 -0500

    Created RolConstraints

---
+++ src/main/java/com/emezon/user/domain/constants/RolConstraints.java
@@ -0,0 +1,12 @@
+package com.emezon.user.domain.constants;
+
+public class RolConstraints {
+
+    public static final String ADMIN = "admin";
+    public static final String AUX_BODEGA = "aux_bodega";
+
+    private RolConstraints() {
+        throw new IllegalStateException("Utility class");
+    }
+
+}
