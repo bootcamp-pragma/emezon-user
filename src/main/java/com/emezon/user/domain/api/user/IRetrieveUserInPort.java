0c6fe590ffbb518ec251ea0dce9c6dd396cfc872
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-09-30 04:00:34 -0500

    Added IRetrieveUserInPort

---
+++ src/main/java/com/emezon/user/domain/api/user/IRetrieveUserInPort.java
@@ -0,0 +1,16 @@
+package com.emezon.user.domain.api.user;
+
+import com.emezon.user.domain.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IRetrieveUserInPort {
+
+    Optional<User> findById(String id);
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findAll(int page, int size, List<String> sorting);
+
+}
