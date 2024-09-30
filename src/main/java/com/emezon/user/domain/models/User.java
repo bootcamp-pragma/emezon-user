d9e13c5eb0e7489b0d1b80316812a7f195ce40fd
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-09-30 03:57:10 -0500

    Updated User model

--- src/main/java/com/emezon/user/domain/models/User.java
+++ src/main/java/com/emezon/user/domain/models/User.java
@@ -12,8 +12,6 @@ public class User {
     private String password;
     private Rol rol;
 
-    private User() { }
-
     public String getId() {
         return id;
     }
@@ -84,64 +82,6 @@ public class User {
 
     public void setRol(Rol rol) {
         this.rol = rol;
-    }
-
-    public static class Builder {
-        private final User user;
-
-        public Builder() {
-            user = new User();
-        }
-
-        public Builder setId(String id) {
-            user.setId(id);
-            return this;
-        }
-
-        public Builder setName(String name) {
-            user.setName(name);
-            return this;
-        }
-
-        public Builder setLastName(String lastName) {
-            user.setLastName(lastName);
-            return this;
-        }
-
-        public Builder setDocNumber(String docNumber) {
-            user.setDocNumber(docNumber);
-            return this;
-        }
-
-        public Builder setCellphone(String cellphone) {
-            user.setCellphone(cellphone);
-            return this;
-        }
-
-        public Builder setBirthDate(String birthDate) {
-            user.setBirthDate(birthDate);
-            return this;
-        }
-
-        public Builder setEmail(String email) {
-            user.setEmail(email);
-            return this;
-        }
-
-        public Builder setPassword(String password) {
-            user.setPassword(password);
-            return this;
-        }
-
-        public Builder setRol(Rol rol) {
-            user.setRol(rol);
-            return this;
-        }
-
-        public User build() {
-            return user;
-        }
-
     }
 
 }
