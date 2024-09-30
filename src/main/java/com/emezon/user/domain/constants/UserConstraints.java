e9ef9feb77453f7dacd0815001a1083f45339a9d
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-09-30 04:16:42 -0500

    Added UserConstraints

---
+++ src/main/java/com/emezon/user/domain/constants/UserConstraints.java
@@ -0,0 +1,10 @@
+package com.emezon.user.domain.constants;
+
+public class UserConstraints {
+
+    public static final int MAX_CELLPHONE_LENGTH = 13;
+    private static final int LEGAL_AGE = 18;
+
+    private UserConstraints() { }
+
+}
