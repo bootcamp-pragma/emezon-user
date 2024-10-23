cbfc0446ef6f457739a5de71355e23e7d7f3990c
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-10-22 22:07:45 -0500

    Created RonEntityCOnstants

---
+++ src/main/java/com/emezon/user/infra/outbound/mysql/jpa/constants/RolEntityConstants.java
@@ -0,0 +1,9 @@
+package com.emezon.user.infra.outbound.mysql.jpa.constants;
+
+public class RolEntityConstants {
+
+    public static final String TABLE_NAME = "roles";
+
+    private RolEntityConstants() {}
+
+}
