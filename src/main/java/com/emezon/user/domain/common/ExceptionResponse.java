23dd5e5bc14b2be5bdb02414d4065d5e3fb4888a
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-10-23 01:35:19 -0500

    Updated ExceptionResponse

--- src/main/java/com/emezon/user/domain/common/ExceptionResponse.java
+++ src/main/java/com/emezon/user/domain/common/ExceptionResponse.java
@@ -1,20 +1,15 @@
 package com.emezon.user.domain.common;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
 public class ExceptionResponse {
     private String errorMessage;
     private String requestedURI;
     private Integer errorCode;
     private String details;
     private LocalDateTime timestamp;
+
+    public ExceptionResponse() {}
 
     public ExceptionResponse(String messages, String description, int value) {
         this.errorMessage = messages;
@@ -29,5 +24,45 @@ public class ExceptionResponse {
         this.errorCode = value;
         this.details = details;
         this.timestamp = LocalDateTime.now();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getRequestedURI() {
+        return requestedURI;
+    }
+
+    public void setRequestedURI(String requestedURI) {
+        this.requestedURI = requestedURI;
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
     }
 }
