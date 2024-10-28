e531927ee1362f8982359eff176df0174bca8351
Jeison Moncayo <131186236+gsondev@users.noreply.github.com>
2024-10-27 21:58:43 -0500

    Revert "Feature/hu007 create user aux bodega"

--- src/main/java/com/emezon/user/infra/outbound/mysql/jpa/constants/RoleEntityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.emezon.user.infra.outbound.mysql.jpa.constants;
-
-public class RoleEntityConstants {
-
-    public static final String TABLE_NAME = "roles";
-
-    private RoleEntityConstants() {}
-
-}
