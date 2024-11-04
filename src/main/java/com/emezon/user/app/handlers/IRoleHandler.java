972bf711d7ef8b3503964a7b6b4017b44fd13ad9
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-11-03 22:58:32 -0500

    Deleted unused import

--- src/main/java/com/emezon/user/app/handlers/IRoleHandler.java
+++ src/main/java/com/emezon/user/app/handlers/IRoleHandler.java
@@ -4,8 +4,6 @@ import com.emezon.user.app.dtos.role.RoleDTO;
 import com.emezon.user.domain.utils.PaginatedResponse;
 import org.springframework.util.MultiValueMap;
 
-import java.util.List;
-
 public interface IRoleHandler {
 
     RoleDTO getRolById(String id);
